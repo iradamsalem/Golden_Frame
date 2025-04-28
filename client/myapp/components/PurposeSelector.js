f13372e3c97da401def8c0ebbd721e3a26ada8bf
Moshik520 <your_email@example.com>
2025-04-28 19:35:53 +0300

    scores

--- client/myapp/components/PurposeSelector.js
+++ client/myapp/components/PurposeSelector.js
@@ -105,7 +105,7 @@ const PurposeSelector = () => {
         onPress={() => {
           setSelectedPurpose(item.id); // Update selected state
         
-          fetch('http://192.162.8.136:3001/api/purpose', {
+          fetch('http://192.168.1.142:3001/api/purpose', {
             method: 'POST',
             headers: {
               'Content-Type': 'application/json',
